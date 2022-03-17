5712b3852846e6a24fed5bbe4ec236e12d13eeb6
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2022-03-16 23:13:18 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,7 +2,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-import { Fancybox, Carousel, } from "@fancyapps/ui";
+import { Carousel } from "@fancyapps/ui";
 const mainTL = gsap.timeline();
 
 window.addEventListener('load', function(){
@@ -31,10 +31,7 @@ function fader() {
 gsap.set("#maintain-content", {alpha:0,  x:-600})
 tl.to("#maintain-content", {alpha:1, x:0, scrollTrigger:{trigger: "#stickout", scrub:true}})
 }
-ScrollTrigger.defaults({
-    toggleActions: "restart pause resume none",
-    markers:true
-})
+
 
 function faderright() {
   const tl = gsap.timeline()
@@ -42,11 +39,10 @@ gsap.set("#stickout-image", {alpha:0,  y:400})
 tl.to("#stickout-image", {alpha:1, y:0, scrollTrigger:{trigger: "#stickout-content h2", scrub:true}})
 }
 
-
-function sheeringfadeup() {
+function sherringfadeup() {
   const tl = gsap.timeline()
-gsap.set("#sheering", {alpha:0,  y:400})
-tl.to("#sheering", {alpha:1, y:0, scrollTrigger:{trigger: "#maintain-content h2", scrub:true}})
+gsap.set("#sheering", {alpha:0, duration:1, y:400})
+tl.to("#sheering", {alpha:1, y:0, scrollTrigger:{trigger: "#sheering-up", scrub:true}})
 }
 
 
@@ -54,5 +50,5 @@ tl.to("#sheering", {alpha:1, y:0, scrollTrigger:{trigger: "#maintain-content h2"
 
 mainTL.add(fadein())
 .add(fader())
-.add(sheeringfadeup())
+.add(sherringfadeup())
 .add(faderright());
